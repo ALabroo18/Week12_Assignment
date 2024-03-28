6643b16ea19d71c13c5370ca67529f480cbcff41
Dormio-san <liam.lovett06@gmail.com>
2024-03-28 15:30:06 -0400

    Create Task 3 script and begin work on it.
    Task three script creates the deck, but it still needs to be randomized.

--- Lab08/Assets/Scripts/Task3.cs
+++ Lab08/Assets/Scripts/Task3.cs
@@ -1,18 +1,64 @@
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Task3 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Stack that stores the cards that are in the deck.
+    private Stack<string> deck = new Stack<string>();
+
+    // Stack that stores the cards that are in the players hands.
+    private Stack<string> hand = new Stack<string>();
+
     void Start()
     {
-        
+        CreateDeck();
     }
 
-    // Update is called once per frame
-    void Update()
+    void CreateDeck()
     {
+        // Add King of each suit.
+        deck.Push("K♠");
+        deck.Push("K♣");
+        deck.Push("K♥");
+        deck.Push("K♦");
         
+        // Add Queen of each suit.
+        deck.Push("Q♠");
+        deck.Push("Q♣");
+        deck.Push("Q♥");
+        deck.Push("Q♦");
+
+        // Add Jack of each suit.
+        deck.Push("J♠");
+        deck.Push("J♣");
+        deck.Push("J♥");
+        deck.Push("J♦");
+
+        // Add Ace of each suit.
+        deck.Push("A♠");
+        deck.Push("A♣");
+        deck.Push("A♥");
+        deck.Push("A♦");
+
+        
+        // Planning to randomize when creating the deck rather than after it's created.
+        // Randomize/shuffle the deck.
+        deck.OrderBy(x => Random.value);
+
+        foreach (string card in deck)
+        {
+            Debug.Log(card);
+        }
+    }
+
+    // Function that checks if the player has three cards from the same suit. If they do, they win.
+    void VictoryCheck()
+    {
+
     }
 }
